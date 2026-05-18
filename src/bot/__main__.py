58d578002c063118380de7c103154d41ed2170eb
ksredkin <ksredkin0968@gmail.com>
2026-05-18 13:45:30 +0300

    fix my best bug of all time...

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -9,6 +9,12 @@ from aiogram.enums import ParseMode
 from aiogram.types import BotCommand, FSInputFile, InputProfilePhotoStatic
 from singbox2proxy import SingBoxProxy
 
+from src.bot.core.config import (
+    BOT_BEFORE_START_DESCRIPTION,
+    BOT_NAME,
+    BOT_PHOTO_PATH,
+    BOT_PROFILE_DESCRIPTION,
+)
 from src.bot.handlers.command import command_router
 from src.bot.middlewares.cache import CacheMiddleware
 from src.common.services.cache import cache
@@ -22,25 +28,20 @@ bot_commands = [BotCommand(command="stats", description="📈 Состояние
 async def setup_bot(bot: Bot) -> None:
     logger.info("Начата настройка бота")
 
-    bot_name = os.getenv("BOT_NAME")
-    bot_before_start_description = os.getenv("BOT_BEFORE_START_DESCRIPTION")
-    bot_profile_description = os.getenv("BOT_PROFILE_DESCRIPTION")
-    bot_photo_path = os.getenv("BOT_PHOTO_PATH")
-
     try:
-        await bot.set_my_name(bot_name)
+        await bot.set_my_name(BOT_NAME)
         logger.info("Имя бота обновлено")
     except Exception as e:
         logger.warning(f"Не удалось настроить имя бота: {e}")
 
     try:
-        await bot.set_my_description(bot_before_start_description)
+        await bot.set_my_description(BOT_BEFORE_START_DESCRIPTION)
         logger.info("Описание бота до start обновлено")
     except Exception as e:
         logger.warning(f"Не удалось настроить описание до start бота: {e}")
 
     try:
-        await bot.set_my_short_description(bot_profile_description)
+        await bot.set_my_short_description(BOT_PROFILE_DESCRIPTION)
         logger.info("Описание бота обновлено")
     except Exception as e:
         logger.warning(f"Не удалось настроить описание бота: {e}")
@@ -52,7 +53,7 @@ async def setup_bot(bot: Bot) -> None:
         logger.warning(f"Не удалось настроить команды бота: {e}")
 
     try:
-        photo = InputProfilePhotoStatic(photo=FSInputFile(bot_photo_path))  # type: ignore
+        photo = InputProfilePhotoStatic(photo=FSInputFile(BOT_PHOTO_PATH))
         await bot.set_my_profile_photo(photo=photo)
         logger.info("Фото бота обновлено")
     except Exception as e:
@@ -62,41 +63,45 @@ async def setup_bot(bot: Bot) -> None:
 
 
 async def main() -> None:
-    token = os.getenv("BOT_TOKEN")
+    try:
+        token = os.getenv("BOT_TOKEN")
 
-    if not token:
-        logger.error("BOT_TOKEN is not set in environment variables!")
-        sys.exit(1)
+        if not token:
+            logger.error("BOT_TOKEN is not set in environment variables!")
+            sys.exit(1)
 
-    proxy = os.getenv("BOT_PROXY")
-    vless_proxy = os.getenv("BOT_VLESS_PROXY")
+        proxy = os.getenv("BOT_PROXY")
+        vless_proxy = os.getenv("BOT_VLESS_PROXY")
 
-    if proxy:
-        session = AiohttpSession(proxy=proxy)
-        logger.info("Запуск бота с proxy")
-    elif vless_proxy:
-        proxy = SingBoxProxy(vless_proxy)
-        proxy.start()
-        session = AiohttpSession(proxy=proxy.socks5_proxy_url)
-        logger.info("Запуск бота с VLESS proxy")
-    else:
-        session = AiohttpSession()
-        logger.info("Запуск бота без proxy")
+        if proxy:
+            session = AiohttpSession(proxy=proxy)
+            logger.info("Запуск бота с proxy")
+        elif vless_proxy:
+            proxy = SingBoxProxy(vless_proxy)
+            proxy.start()
+            session = AiohttpSession(proxy=proxy.socks5_proxy_url)
+            logger.info("Запуск бота с VLESS proxy")
+        else:
+            session = AiohttpSession()
+            logger.info("Запуск бота без proxy")
 
-    properties = DefaultBotProperties(parse_mode=ParseMode.HTML)
+        properties = DefaultBotProperties(parse_mode=ParseMode.HTML)
 
-    bot = Bot(token, session, properties)
-    await setup_bot(bot)
+        bot = Bot(token, session, properties)
+        await setup_bot(bot)
 
-    dp = Dispatcher()
-    cache_middleware = CacheMiddleware(cache)
+        dp = Dispatcher()
+        cache_middleware = CacheMiddleware(cache)
 
-    dp.message.middleware(cache_middleware)
+        dp.message.middleware(cache_middleware)
 
-    dp.include_router(command_router)
+        dp.include_router(command_router)
 
-    await dp.start_polling(bot)
+        await dp.start_polling(bot)
+    except Exception as e:
+        logger.critical(f"Error occured: {e}")
+        raise
 
 
-if __name__ == "__init__":
+if __name__ == "__main__":
     asyncio.run(main())
